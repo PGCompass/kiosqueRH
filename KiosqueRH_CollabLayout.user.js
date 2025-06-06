0c43666f4c02d6f10dea9e6a5c53c263660831bf
PGCompass <pierre.gardie@compass-group.fr>
2025-06-06 09:56:16 +0200

    Update KiosqueRH_CollabLayout.user.js

--- KiosqueRH_CollabLayout.user.js
+++ KiosqueRH_CollabLayout.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         KiosqueRH - Mise en page collaborateurs
-// @version      3.0
+// @version      3.1
 // @description  Reorder <tr> elements in the ProdTable within the 'colonne' div based on a predefined list of priorities
 // @author       Pierre GARDIE - Compass Group France
 // @match        https://hr-services.fr.adp.com/*
